e3ec9ce702db32e583f7a58ebb24077e885242e6
VirendharS <virendhar@users.noreply.github.com>
2014-08-24 18:15:41 +0530

    Updated with the code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,68 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Code: Written by Virendhar Sivaraman on 24 th Aug 2014 for Cloudera R Programming Week 2 Programming Assignemnt 
+# Description :
 
-## Write a short comment describing this function
+# Matrix inversion is usually a costly computation and there may be some benefit
+# to caching the inverse of a matrix rather than compute it repeatedly. The
+# following two functions are used to cache the inverse of a matrix.
+
+
+# Code : makeCacheMatrix() Function does the following, 
+# Description :  The function is used by the cacheSolve() to Set & Get value of the matrix and to set & Get 
+#                inverse of the matrix
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) inv <<- inverse
+        getinverse <- function() inv
+        list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
+# Code : cacheSove() Function. 
+# Description : The function returns the inverse of the matrix. It first checks if
+#               the inverse has already been computed. If so, it gets the result and skips the
+#               computation. If not, it computes the inverse, sets the value in the cache via setinverse function.
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinverse()
+        if(!is.null(inv)) {
+                message("getting cached data.")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data)
+        x$setinverse(inv)
+        inv
 }
+
+
+## Sample run:
+## > x = rbind(c(1, -0.25), c(-0.25, 1))
+## > SqMatrix = makeCacheMatrix(x)
+## > SqMatrix$get()
+##       [,1]  [,2]
+## [1,]  1.00 -0.25
+## [2,] -0.25  1.00
+
+## No cache in the first run
+## > cacheSolve(SqMatrix)
+##           [,1]      [,2]
+## [1,] 1.0666667 0.2666667
+## [2,] 0.2666667 1.0666667
+
+## Retrieving from the cache in the second run
+## > cacheSolve(SqMatrix)
+## getting cached data.
+##           [,1]      [,2]
+## [1,] 1.0666667 0.2666667
+## [2,] 0.2666667 1.0666667
+## > 
+
+
+
